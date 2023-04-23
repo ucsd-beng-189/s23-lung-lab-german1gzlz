0c56eb5be25a39799d2c9210c1d9dba36671c275
German Gonzalez <g6gonzalez@ucsd.edu>
2023-04-22 22:20:26 -0700

    Task 3
    Code and PPvsBeta.jpg plot

--- lung.m
+++ lung.m
@@ -2,6 +2,21 @@
 clear all
 clf
 global Pstar cstar n maxcount M Q camax RT cI;
-setup_lung
-cvsolve
-outchecklung
+for i = 0:.1:10
+    beta = .1*i;
+    setup_lung
+    cvsolve
+    outchecklung
+
+    %Plots Partial PRessures vs. Beta
+    figure(4)
+    plot(i/10,PI,"bo")
+    hold on
+    plot(i/10,PAbar,"ro")
+    plot(i/10,Pabar,"go")
+    plot(i/10,Pv,"co")
+    legend("Inspired PP","Mean Alveolar PP","Mean Arterial PP","Venous PP")
+    xlabel("Beta")
+    ylabel("Partial Pressure")
+    title("Partial Pressure vs. Beta")
+end
